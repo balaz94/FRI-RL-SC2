591da9fe9d13dba2b08fa5298c25156f6fdbfee9
František Chlustina <frantisek.chlustina@asseco-ceit.com>
2022-04-22 09:42:23 +0200

    Update

--- sc2_marines_vs_zealot.py
+++ sc2_marines_vs_zealot.py
@@ -14,6 +14,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import graph.make_graph
 from agents.ppo_parallel import Agent
+import graph.make_graph as graph
 import datetime
 import os
 import signal
@@ -53,85 +54,44 @@ def learning(count_of_iterations):
     # signal.signal(signal.SIGTERM, at_exist)
     # signal.signal(signal.SIGINT, at_exist)
 
-    count_of_processes = 3
-    count_of_envs = 2
+    count_of_processes = 1
+    count_of_envs = 1
     count_of_steps = 1024
     batch_size = 1024
     count_of_epochs = 4
     first_iteration = 0
     input_dim = (29, 64, 64)
 
+    graph.scatter_plot("results/marines_vs_zealot/data/ppoF1.txt")
+    graph.scatter_plot("results/marines_vs_zealot/data/ppoF2.txt")
     agent.train("", Env, count_of_processes, count_of_envs, count_of_iterations, count_of_steps, batch_size, count_of_epochs, first_iteration, input_dim)
+    #agent.load_model("results/marines_vs_zealot/models/ppoF1.pt")
+    #agent.load_model("results/marines_vs_zealot/models/ppoF2.pt")
 
-   # workers = []
-    #for id in range(10):
-     #   env = Env()
-     #   w = Worker(id, env, agent)
-     #   workers.append(w)
+def animation():
+    model = PolicyValueModel(27, 800)  # shards
+    model.to("cuda")
 
-    #agent.learn(workers, 764, 10001, 764, batches_count = 8, max_mc = 10)
+    model.load_state_dict(torch.load("results/marines_vs_zealot/models/ppoF2.pt"))
 
-    #total_time_file.close()
+    env = Env(*"")
+    i = 0
+    while i < 10:
+        terminal = False
+        i += 1
+        observations = torch.from_numpy(env.reset())
+        while not terminal:
+            with torch.no_grad():
+                observations = observations.to("cuda").unsqueeze(0).float()
+                logits, values = model(observations)
 
-# def animation():
-#     scores = []
-#     avg_scores = deque([])
-#     actions = 27
-#     env = Env()
-#
-#     agent = AgentPPO(0.99, actions, Net(), 0.001, beta_entropy=0.01, id=0, name='sc2_custom_ZealotArmorUp_re_mc_10_ntr', depth = 21, height = 64, width = 64)
-#     agent.model.load_state_dict(torch.load('models/sc2_custom_ZealotArmorUp_re_mc_10_ntr_0_ppo.pt'))
-#
-#     i = 0
-#     while True:
-#         score = 0
-#         terminal = False
-#         state = env.reset()
-#         i += 1
-#
-#         while not terminal:
-#             time.sleep(0.25)
-#             action = agent.choose_action(torch.from_numpy(state).double())
-#             state, reward, terminal, _ = env.step(action)
-#             score += reward
-#
-#         avg_scores.append(score)
-#         if len(avg_scores) > MAX_SCORE_COUNT:
-#             avg_scores.popleft()
-#
-#         scores.append(score)
-#         print('episode: ', i, '\t\tavg-100 score:',
-#               round(np.average(avg_scores), 3))
+                probs = F.softmax(logits, dim=-1)
+                action = probs.multinomial(num_samples=1)
 
+            new_state, _, terminal = env.step(action[0, 0].item())
+            observations = torch.from_numpy(new_state)
 
-# def at_exist(signum, frame):
-#     global score_file
-#     global steps_file
-#     global total_time_file
-#     global START_TIME
-#     global agent
-#
-#     total_time_file.write("Train time: " + str(datetime.datetime.now() - START_TIME) + '\n')
-#     total_time_file.write("Episodes: " + str(agent.get_episodes()) + '\n')
-#     total_time_file.close()
-#     score_file.flush()
-#     steps_file.flush()
-#     score_file.close()
-#     steps_file.close()
-
-
-
-# class Stat:
-#     def __init__(self, score_file, steps_file):
-#         self.score_file = score_file
-#         self.steps_file = steps_file
-#         self.i = 0
-#
-#     def save_stat(self, env, score):
-#         self.i += 1
-#         self.score_file.write(str(score) + '\n')
-#         self.steps_file.write(str(env.get_raw_obs().observation.game_loop[0]) + '\n')
 
 if __name__ == '__main__':
-    learning(2000)
-    #animation()
+    #learning(2000)
+    animation()
